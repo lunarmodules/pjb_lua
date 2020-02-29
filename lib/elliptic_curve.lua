6f860c67df66d5364d896c5cefd2088b33b18ffe
Peter Billam <pjb@pjb.com.au>
2020-02-29 11:46:14 +1100

    rewrite *_rat using rational arithmethic

--- lib/elliptic_curve.lua
+++ lib/elliptic_curve.lua
@@ -78,6 +78,24 @@ function mod_div (i, j, p)
 	return mul(i, inverse(j,p), p)
 end
 
+local infty = 'infty'   -- the point at infinity
+
+function y_gen_modp (a, b, p)
+	-- elliptic equation is y^2 = x^3 + a*x + b
+	local pp1by4 = (p + 1) / 4   -- but p==17 ?
+	return function (xp)   -- (
+		xp = xp%p
+		local y_squared = (x*(x%p)%p + (a*x)%p + b)%p
+		-- https://en.wikipedia.org/wiki/Modular_square_root
+		-- https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+		-- https://math.stackexchange.com/questions/633160/modular-arithmetic-find-the-square-root
+		--   suppose you want to find (x)^(1/2) mod p(prime) then simply
+		--   calculate (x)^((p+1)/4) mod p.   Eg: in your case x=3 and p=11,
+		--   3^((11+1)/4)=27= 5 mod 11. similarly -5 will be a root.
+		-- https://math.stackexchange.com/questions/1895058/how-to-find-modulus-square-root?rq=1
+	end
+end
+
 function add_gen_modp (a, b, p)
 	-- a closure-generator add_gen_modp, because p,a,b rarely change
 	-- elliptic equation is y^2 = x^3 + a*x + b
@@ -89,7 +107,6 @@ function add_gen_modp (a, b, p)
 	-- and at 11:25 he say a point on the curve is an ordered pair.
 	-- see 12:20 for the example! 13:25 "remember, we're computing in mod 17"!
 	-- so use mod_div()    20200226 passes first test :-)
-	local infty = 'infty'   -- the point at infinity
 	return function (xp, yp, xq, yq)   -- (
 		if yp == infty then return xq,yq end
 		if yq == infty then return xp,yp end
@@ -195,32 +212,43 @@ function add_gen_rat (a, b)
 	local infty = 'infty'   -- the point at infinity
 	return function (xp, yp, xq, yq)
 		-- use rational.lua! a,b integer; xp,yp, xq,yq, s, xr,yr all rational
-		if yp == infty then return xq,yq end
-		if yq == infty then return xp,yp end
-		if xp == xq then
-			if yp == yq then   -- same point; use the tangent
-				local s  = ((3*xp*xp)%p + a) / (2*yp)  -- 04:28
-				local xr = s*s - 2*xp                  -- 04:40
-				local yr = s*(xp-xr) - yp              -- 04:51
-				return xr % p, yr % p
+		-- NO! a and b can be rational !
+		--     (only in Ash and Gross must a,b be integers !)
+		-- the rest of this all needs replacing !
+		if yp[1] == infty then return xq,yq end
+		if yq[1] == infty then return xp,yp end
+		if RA.eq(xp,xq) then
+			if RA.eq(yp,yq) then   -- same point; use the tangent
+			-- ((3*xp*xp)%p + a) / (2*yp)  -- 04:28
+				local s = RA.div(
+				  RA.mul({3,1}, RA.mul(xp,xp)),  RA.mul({2,1}, yp)
+				)
+				-- s*s - 2*xp                  -- 04:40
+				local xr = RA.sub(RA.mul(s,s) - RA.mul({2,1},xp))
+				-- s*(xp-xr) - yp              -- 04:51
+				local yr = RA.sub(RA.mul(s, (RA.sub(xp,xr))), yp)
+				return xr, yr
 			else
-				return 0,infty
+				return {1,1}, {infty,1}
 			end
 		end
-		local s  = (yq-yp) / (xq-xp) -- xq,yq are integers so s is rational
-		local xr = s*s - (xp+xq)   -- 03:40
-		local yr = s*(xp-xr) - yp  -- 03:43
-		return xr % p, yr % p
+		-- (yq-yp) / (xq-xp)
+		local s  = RA.div(RA.sub(yq,yp), RA.sub(xq,xp))
+		-- s*s - (xp+xq)   -- 03:40
+		local xr = RA.sub(RA.mul(s,s), RA.add(xp,xq))
+		-- s*(xp-xr) - yp  -- 03:43
+		local yr = RA.sub(RA.mul(s,RA.sub(xp,xr)), yp)
+		return xr, yr
 	end
 end
 
-function scalarmul_gen_rat (a, b, p)
-	-- a closure-generator add_gen_rat, because p,a,b rarely change
+function scalarmul_gen_rat (a, b)
+	-- a closure-generator add_gen_rat, because a,b rarely change
 	-- elliptic equation is y^2 = x^3 + a*x + b  mod p
 	return function (xp, yp, k)   -- 06:00
 		local total_x = xp
 		local total_y = yp
-		local addfunc = add_gen_rat (a, b, p)
+		local addfunc = add_gen_rat (a, b)
 		for i = 2,k do
 			total_x, total_y = addfunc(total_x, total_y, xp, yp)
 		end
@@ -246,6 +274,7 @@ function M.set_numberfield (s)
 		if not RA then RA = require 'rational' end
 		-- if xp,yp, xq,yq are rational then so is the gradient, and xr,yr
 		-- but even if x is rational, y involves srqt and thus must be real!
+		M.y_gen         = y_gen_rat
 		M.add_gen       = add_gen_rat
 		M.scalarmul_gen = scalarmul_gen_rat
 		return true
@@ -259,6 +288,7 @@ function M.set_numberfield (s)
 		M.scalarmul_gen = scalarmul_gen_complex
 		return true
 	elseif s == 'Z/pZ' or string.find(s, '^mod') then
+		M.y_gen         = y_gen_modp
 		M.add_gen       = add_gen_modp
 		M.scalarmul_gen = scalarmul_gen_modp
 		return true
@@ -289,16 +319,56 @@ in Z/pZ (modular arithmetic), R (real numbers) and Q (rational numbers)
 
 =head1 FUNCTIONS
 
+gcd (small, big)   -- consult urls.html
+inverse (a, n) -- see urls.html "extended Euclidean algorithm"
+mul (a, b, n)
+mod_div (i, j, p)
+add_gen_modp (a, b, p)
+	function (xp, yp, xq, yq)   -- (
+scalarmul_gen_modp (a, b, p)
+	function (xp, yp, k)   -- 06:00
+y_gen_real (a, b)
+	function (x)
+add_gen_real (a, b)
+	return function (xp, yp, xq, yq)
+scalarmul_gen_real (a, b)
+	return function (xp, yp, k)   -- 06:00
+add_gen_rat (a, b)
+	return function (xp, yp, xq, yq)
+scalarmul_gen_rat (a, b, p)
+	return function (xp, yp, k)   -- 06:00
+M.set_numberfield (s)
+
 =over 3
 
-=item I<ttest(a,b, hypothesis)>
+=item I<gcd (small, big)>
 
 The arguments I<a> and I<b> are arrays of numbers
-
 The I<hypothesis> can be one of 'a>b', 'a<b', 'b>a', 'b<a',
 'a~=b' or 'a<b'.
-
 I<ttest> returns the probability of your hypothesis being wrong.
+
+=item I<inverse (a, n)>
+
+=item I<add_gen_modp (a, b, p)>
+
+=item I<scalarmul_gen_modp (a, b, p)>
+
+=item I<y_gen_real (a, b)>
+
+=item I<add_gen_real (a, b)>
+
+=item I<scalarmul_gen_real (a, b)>
+
+=item I<add_gen_rat (a, b)>
+
+=item I<scalarmul_gen_rat (a, b, p)>
+
+=item I<set_numberfield(str)>
+
+if I<str> is "C<R>" or "C<real>"
+if I<str> is "C<Q>" or "C<rational>"
+if I<str> is "C<Z/pZ>" or "C<modular>"
 
 =back
 
